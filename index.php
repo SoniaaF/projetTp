2adf12382024cf8b4149e1a612820fd65a6b3e71
sonia <sonia.ferkous@gmail.com>
2019-08-07 08:48:19 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Institut Picard de Langues</title>
+    </head>
+    <body>
+        <div class="container col-8">
+            <h1 class="text-center">Institut Picard de Langues</h1>
+        </div>
+        <img src="assets/img/fr.png" alt="Drapeau français, passer le site en français">
+        <img src="assets/img/en.png" alt="Drapeau anglais, passer le site en anglais">
+        <nav class="navbar sticky-top navbar-dark bg-dark">
+            <a class="navbar-brand" href="#!">Accueil</a>
+            <a class="navbar-brand" href="#!">Informations</a>
+            <a class="navbar-brand" href="#!">Test</a>
+            <a class="navbar-brand" href="#!">Inscription / Connexion</a>
+            <a class="navbar-brand" href="#!">Nous contacter</a>
+        </nav>
+
+    </body>
+</html>
